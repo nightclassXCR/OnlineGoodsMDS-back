55ab095c99a8589aac60e78435d9131a8d3c2af0
nightclassXCR <1246716434@qq.com>
2025-06-03 16:03:02 +0800

    feat:build basic env

---
+++ src/main/java/com/dd/onlinegoodsms/OnlineGoodsMsApplication.java
@@ -0,0 +1,13 @@
+package com.dd.onlinegoodsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineGoodsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineGoodsMsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dd/onlinegoodsms/OnlineGoodsMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dd.onlinegoodsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineGoodsMsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
